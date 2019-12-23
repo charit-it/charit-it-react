832895eb1a4d065272d57141ec49634fece33cf2
peaceoni <peaceoni49@yahoo.com>
2019-12-23 18:51:23 +0000

    1st react imput

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './Nav.js';
+import Footer from "./Footer.js"
+
+import"./App.css";
+
+class App extends React.Component {
+  render () {
+    return (
+
+   <Nav />
+  
+   );
+  }
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render () {
+        return (
+            <div class="nav">
+            
+            <footer>
+       <ul>
+           <li class="="active><a href="#"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>
+           <li><a href="#"><i class="fa fa-laptop"></i>Eletronics</a></li>
+           <li><a href="#"><i class="fa fa-child"></i> Children</a></li>
+           <li><a href="#"><i class="fa fa-male" ></i>Male</a></li>
+           <li><a href="#"><i class="fa fa-female"></i>Women</a></li>
+           <li><a href="#"><i class="fa fa-phone" ></i>About-us</a></li>
+           
+       </ul>
+      </footer>
+       
+        
+      
+    
+</div>
+        );
+    }
+
+}
+
+export default Footer;
