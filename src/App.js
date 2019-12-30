1e44e71a636622862936969222565d9da3124f94
Caio <caioymc@live.com>
2019-12-30 22:56:24 +0000

    Fixed navbar using React-bootstrap lib, Favicon and dropdown menus

--- src/App.js
+++ src/App.js
@@ -1,71 +1,45 @@
 import React from 'react';
-import Items from './Items.js'
+import * as RB from 'react-bootstrap';
+import Items from './Items.js';
+import Logo from './images/logo.png';
 import "./App.css";
 
 class App extends React.Component {
   render () {
     return (
-      <section>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTog">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <a className="navbar-brand" href="index.html"><h4><span id="title">Charit-</span>It</h4></a>
-          <div className="collapse navbar-collapse" id="navbarTog">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li className="nav-item">
-                <a className="nav-link nav" href="."> Electronics</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav" href="."> Children</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav" href="."> Men</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav" href="."> Women</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav" href="."> About us</a>
-              </li>
-            </ul>
-            <form className="form-inline my-2 my-lg-0 form-search">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            <ul className="navbar-nav mt-2 mt-lg-0">
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="." id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Sign up/in</a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href=".">Sign up</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href=".">Log in</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <section>
-          <div className="row item-row">
-            <Items />
-            <Items />
-            <Items />
-          </div>
-          <div className="row item-row">
-            <Items />
-            <Items />
-            <Items />
-          </div>
-          <div className="row item-row">
-            <Items />
-            <Items />
-            <Items />
-          </div>
-        </section>
-        <footer>
+    <section>
+      <RB.Navbar bg="white" expand="lg">
+        <RB.Navbar.Brand href="index.html">
+          <img alt="" src={Logo} width="30" height="30" className="d-inline-block align-top" />
+          {' '}
+          <b>Charit-</b><b id="title">It</b>
+        </RB.Navbar.Brand>
+        <RB.Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <RB.Navbar.Collapse id="basic-navbar-nav">
+          <RB.Nav className="mr-auto">
+            <RB.Nav.Link href="index.html">Home</RB.Nav.Link>
+            <RB.NavDropdown title="Clothing / Shoes" id="basic-nav-dropdown">
+              <RB.NavDropdown.Item href="#action/3">Children</RB.NavDropdown.Item>
+              <RB.NavDropdown.Item href="#action/3.1">Men</RB.NavDropdown.Item>
+              <RB.NavDropdown.Item href="#action/3.3">Women</RB.NavDropdown.Item>
+            </RB.NavDropdown>
+            <RB.Nav.Link href="#link">Electronics</RB.Nav.Link>
+            <RB.Nav.Link href="#link">About us</RB.Nav.Link>
+            <RB.NavDropdown title="Login" id="basic-nav-dropdown">
+              <RB.NavDropdown.Item href="#action/4.1">Sign up</RB.NavDropdown.Item>
+              <RB.NavDropdown.Divider />
+              <RB.NavDropdown.Item href="#action/4.4">Sign in</RB.NavDropdown.Item>
+            </RB.NavDropdown>
+          </RB.Nav>
+          <RB.Form inline>
+            <RB.FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <RB.Button variant="outline-success">Search</RB.Button>
+          </RB.Form>
+        </RB.Navbar.Collapse>
+      </RB.Navbar>
 
-        </footer>
-      </section>
+    </section>
+    
     );
   }
 }
