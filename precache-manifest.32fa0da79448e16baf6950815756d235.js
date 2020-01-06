cbce604d6ae959ffb6fb25f90ed1e1d0b0a7deb6
Caio <caioymc@live.com>
2020-01-06 15:11:25 +0000

    Updates

---
+++ precache-manifest.32fa0da79448e16baf6950815756d235.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8157f5f0ab6267b39ffc7ae34fd18cda",
+    "url": "/charit-it-react/index.html"
+  },
+  {
+    "revision": "ed1b92b63ab98179676c",
+    "url": "/charit-it-react/static/css/main.8e44f2d4.chunk.css"
+  },
+  {
+    "revision": "0c888a44330191a1666e",
+    "url": "/charit-it-react/static/js/2.75d9e9ea.chunk.js"
+  },
+  {
+    "revision": "21d51257448e9d8566e72d4c9eadc822",
+    "url": "/charit-it-react/static/js/2.75d9e9ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed1b92b63ab98179676c",
+    "url": "/charit-it-react/static/js/main.561e3a77.chunk.js"
+  },
+  {
+    "revision": "0d4c48081bf4e8e99e6e",
+    "url": "/charit-it-react/static/js/runtime-main.2fea1fa9.js"
+  },
+  {
+    "revision": "8130170592879e52d4266a46dbf4415a",
+    "url": "/charit-it-react/static/media/background.81301705.jpg"
+  },
+  {
+    "revision": "6dec3a817c7f9651e265a48c7b4a663a",
+    "url": "/charit-it-react/static/media/logo.6dec3a81.png"
+  }
+]);
